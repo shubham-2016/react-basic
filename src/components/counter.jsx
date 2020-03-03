642b09dbf117e18761e1634577de263bc5624abc
Shubham Yelne <shubham.yelne@neosofttech.com>
2020-03-03 15:52:39 +0530

    handleReset and handleIncrement.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
-    state = {
-        count : this.props.value,
-        file : 'sample',
-        tags : ['tag1','tag2','tag3']
-    };
+    // state = {
+    //     count : this.props.value,
+    //     file : 'sample',
+    //     tags : ['tag1','tag2','tag3']
+    // };
 
     styles = {
         fontSize: '20',
@@ -23,7 +23,7 @@ class Counter extends Component {
             <React.Fragment>
                 <h4>{this.props.title}</h4>
                 
-                <button onClick={ () => { this.handledIncrement(1) } } className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={ () => { this.props.handledIncrement(this.props.counter) } } className="btn btn-secondary btn-sm">Increment</button>
                 
                 <button onClick={ () => this.props.onDelete(this.props.counterId) } className="btn btn-danger btn-sm m-2">Delete</button>
                 
@@ -40,28 +40,23 @@ class Counter extends Component {
     }
 
     getTags(){
-        if(this.state.tags.length === 0){
+        if(this.props.tags.length === 0){
             return 'Sorry There is no tag.';
         }else{
-            return this.state.tags.map((tag) => <li key={tag}>{tag}</li>);
+            return this.props.tags.map((tag) => <li key={tag}>{tag}</li>);
         }
     }
 
     getBadgeClasses(){
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.props.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
-        return this.state.count === 0 ?  "Zero" : count;
+        const { value } = this.props;
+        return this.props.value === 0 ?  "Zero" : value;
     }
-
-    handledIncrement = (num) => {
-        this.setState({ count: this.state.count + num });
-    }
-
 }   
  
 export default Counter;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,43 +4,55 @@ import Counter from './counter.jsx';
 class Main extends Component {
     state = {  
         counters: [
-            {id:1, value:0},
+            {id:1, value:1},
             {id:2, value:0},
             {id:3, value:0},
             {id:4, value:0},
-        ]
+        ],
+        tag: ['tag1','tag2','tag3']
     }
 
     render() { 
         return ( 
             <div>
-                <button onClick={() => { this.handleReset() }} class="btn btn-primary btn-sm m-2">RESET</button>
+                <button onClick={() => { this.handleReset() }} className="btn btn-primary btn-sm m-2">RESET</button>
                 
                 { this.state.counters.map((countr) => 
                     <Counter 
                         key={countr.id} 
                         onDelete={this.handleDelete}
+                        handledIncrement={this.handledIncrement}
                         value={countr.value} 
                         title={'Title: '+countr.id}
                         counterId={countr.id}
+                        tags = {this.state.tag}
+                        counter = {countr}
                     />
                 )} 
             </div>
         );
     }
 
-    handleReset = () => {
-        const counterReset = this.state.counters.map(c => {
-            c.value = 0
+    handleReset = () => {    
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
             return c;
-        });
-        this.setState({counterReset});
+        });    
+
+        this.setState({ counters});
     };
 
     handleDelete = counterId => {
         const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters });
+        this.setState({ counters:counters });
     };
+
+    handledIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index].value++;
+        this.setState({counters});
+    }
 }
  
 export default Main;
